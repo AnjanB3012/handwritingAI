33f0cc706a3504d81b3e0893cb879dc9b0cedac3
Anjan Bellamkonda <147215718+AnjanB3012@users.noreply.github.com>
2025-11-01 15:48:57 -0400

    Created tempalte app for data collection

---
+++ dataApp/handwritingApp/handwritingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  handwritingApp
+//
+//  Created by Anjan Bellamkonda on 11/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dataApp/handwritingApp/handwritingApp/handwritingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  handwritingAppApp.swift
+//  handwritingApp
+//
+//  Created by Anjan Bellamkonda on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct handwritingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
